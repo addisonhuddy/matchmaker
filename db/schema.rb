f01fd945ffaffc7f7e8e28cf046b7054cecfd9f7
Addison Huddy <addisonhuddy@gmail.com>
2016-03-13 12:55:25 -0700

    postgres up and running

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160310033011) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.datetime "created_at", null: false
