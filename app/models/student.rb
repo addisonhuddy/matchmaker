5239573397889e603b874aa48b5ff6a7e9b29ea4
Addison Huddy <addisonhuddy@gmail.com>
2016-03-27 17:44:35 -0700

    form validates and error messages

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,16 @@
 class Student < ActiveRecord::Base
+  validates_presence_of(:first_name,
+                        :time_zone,
+                        :gatech_class,
+                        :gatechuname,
+                        :gatechid,
+                        :email,
+                        :learning_objectives,
+                        :preferred_tools,
+                        :myers_briggs,
+                        :week_preferred,
+                        :weekend_preferred)
 
+  validates_uniqueness_of :gatechid, :email, :gatechuname
+  
 end
